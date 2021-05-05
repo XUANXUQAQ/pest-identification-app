76e90a2ef500aaea8d6984915370ec5467a4e9a2
xuanxu <xuanxusb@outlook.com>
2021-05-05 19:03:46 +0800

    提交

--- src/utils/networkUtils.js
+++ src/utils/networkUtils.js
@@ -1,21 +1,3 @@
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  const expires = `expires=${d.toGMTString()}`;
-  document.cookie = `${cname}=${cvalue}; ${expires}`;
-}
-
-function getCookie(cname) {
-  const name = `${cname}=`;
-  const ca = document.cookie.split(';');
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < ca.length; i++) {
-    const c = ca[i].trim();
-    if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
-  }
-  return '';
-}
-
 const networkUtils = {
   args: {
     form(args) {
@@ -25,25 +7,6 @@ const networkUtils = {
         formData.append(key, args[key]);
       });
       return formData;
-    },
-  },
-  token: {
-    setToken(token) {
-      setCookie('token', token, 1);
-    },
-    removeToken() {
-      setCookie('token', '', 1);
-    },
-    getToken() {
-      return getCookie('token');
-    },
-  },
-  username: {
-    setUsername(username) {
-      setCookie('username', username, 1);
-    },
-    getUsername() {
-      return getCookie('username');
     },
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,75 +1,16 @@
 <template>
-  <el-container class="el-body">
-    <el-main>
-      <router-view/>
-    </el-main>
+  <el-container>
+
   </el-container>
 </template>
 
 <script>
-import networkUtils from '@/utils/networkUtils';
-
 export default {
   name: 'Home',
   components: {},
-  data() {
-    return {
-      path: '',
-      name: '',
-      username: networkUtils.username.getUsername(),
-      sideBarHeight: '100vh',
-    };
-  },
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    setPathAndName(path, name) {
-      this.path = path;
-      this.name = name;
-    },
-    logout() {
-      networkUtils.token.removeToken();
-      this.$router.push({
-        path: '/Login',
-      }).catch(() => {});
-    },
-  },
 };
 </script>
 
 <style lang="scss" scoped>
 
-.el-body {
-  height: 90vh;
-}
-
-.title {
-  font-size: 35px;
-}
-
-.el-aside {
-  background-color: #304156;
-  overflow: hidden;
-}
-
-.el-main {
-  overflow-x: hidden;
-}
-
-.svg-container {
-  padding: 6px 5px 6px 5px;
-  vertical-align: middle;
-  width: 18px;
-  display: inline-block;
-}
-
-.user {
-  font-size: 18px;
-  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
-  position:relative;left:50%;-webkit-transform:translateX(-50%);transform:translateX(-50%);
-}
 </style>
