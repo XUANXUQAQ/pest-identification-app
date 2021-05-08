9bb8c1b8cf666445a81994303c13b2087c07bfbe
xuanxu <xuanxusb@outlook.com>
2021-05-08 19:02:56 +0800

    添加开屏介绍

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,22 @@
       </router-link>
     </mt-header>
 
+    <div style="
+      position: fixed;
+      text-align: center;
+      top: 40vw;"
+    >
+      <svg-icon style="width: 90%; height: 90%" icon-class="identify"></svg-icon>
+
+      <div class="title">
+        林业害虫识别
+      </div>
+
+      <div class="description">
+        通过对大数据、智能识别、深度学习等技术的应用，针对常见林业害虫通过应用手机拍摄或选取相册中的害虫图像，经分析处理后可智能识别害虫种类、识别率、个数等信息。
+      </div>
+    </div>
+
     <div>
       <div style="height: 90vh">
         <div v-show="hasContent">
@@ -90,5 +106,17 @@ export default {
 .hasContent {
   padding: 12px;
   display: flex;
+}
+.description {
+  font-size: 14px;
+  text-shadow: 0 0 2px black;
+  color: black;
+  margin: 10px;
+}
+.title {
+  font-size: 40px;
+  text-shadow: 5px 5px 5px black, 0 0 2px black;
+  color: white;
+  margin-bottom: 20px;
 }
 </style>
