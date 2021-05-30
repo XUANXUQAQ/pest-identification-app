0d33344551434f0f5300b9da9e23314192276297
xuanxu <xuanxusb@outlook.com>
2021-05-30 16:15:48 +0800

    修改后端url

--- src/network/baseURLs.js
+++ src/network/baseURLs.js
@@ -1,6 +1,6 @@
 const baseURLs = {
-  yoloURL: 'http://183.195.216.150:8899',
-  databaseURL: 'http://rjgc.club:8099',
+  yoloURL: 'http://120.79.146.15:8082',
+  databaseURL: 'http://120.79.146.15:8081',
   yoloLocalURL: 'http://localhost:8888',
   databaseLocalURL: 'http://localhost:8889',
 };
